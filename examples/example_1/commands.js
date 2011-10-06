ac0aa9e7e017caa77b3acdcf183dcc010ac889d6
Martin Kaistra <admin@djfun.de>
2011-10-06 12:03:34 +0200

    added an example

---
+++ examples/example_1/commands.js
@@ -0,0 +1,1 @@
+document.write(ChartHelper.render(myTree, 4));
---
+++ examples/example_1/data.js
@@ -0,0 +1,144 @@
+var myTree = {
+  pedigree: {
+    data: {
+      name: "Me",
+    },
+    children: [
+      {
+        data: {
+          name: "Child 1"
+        }
+      },
+      {
+        data: {
+          name: "Child 2"
+        }
+      },
+    ],
+  },
+  siblings2: [
+    {
+      data: {
+        name: "Brother 1"
+      }
+    },
+    {
+      data: {
+        name: "Brother 2"
+      },
+      children: [
+        {
+          data: {
+            name: "Brother 2s daughter"
+          }
+        },
+        {
+          data: {
+            name: "Brother 2s daughter"
+          }
+        },
+        {
+          data: {
+            name: "Brother 2s son"
+          }
+        }
+      ]
+    },
+  ],
+  mother_siblings: [
+    {
+      data: {
+        name: "Aunt 1"
+      },
+      children: [
+        {
+          data: {
+            name: "Aunt 1s son"
+          }
+        },
+        {
+          data: {
+            name: "Aunt 1s son"
+          }
+        },
+      ]
+    },
+    {
+      data: {
+        name: "Aunt 2"
+      },
+      children: [
+        {
+          data: {
+            name: "Aunt 2s son"
+          }
+        },
+        {
+          data: {
+            name: "Aunt 2s daughter"
+          }
+        },
+        {
+          data: {
+            name: "Aunt 2s son"
+          }
+        },
+        {
+          data: {
+            name: "Aunt 2s son"
+          }
+        },
+        {
+          data: {
+            name: "Aunt 2s daughter"
+          }
+        },
+        {
+          data: {
+            name: "Aunt 2s daughter"
+          }
+        },
+      ]
+    },
+  ],
+  father: {
+    data: {
+      name: "Father"
+    }
+  },
+  mother: {
+    data: {
+      name: "Mother"
+    },
+    father: {
+      data: {
+        name: "Grandfather"
+      },
+      father: {
+        data: {
+          name: "Great-grandfather"
+        },
+      },
+      mother: {
+        data: {
+          name: "Great-grandmother"
+        }
+      }
+    },
+    mother: {
+      data: {
+        name: "Grandmother"
+      },
+      father: {
+        data: {
+          name: "Great-grandfather"
+        },
+      },
+      mother: {
+        data: {
+          name: "Great-grandmother"
+        }
+      }
+    },
+  },
+};
